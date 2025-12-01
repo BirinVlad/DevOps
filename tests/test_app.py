78b2c5c6f0bbd8e2ceb634edaa392b7397f4d596
BirinVlad <90749149+BirinVlad@users.noreply.github.com>
2025-12-01 13:22:43 +0200

    tests structure

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+from ..app import add, is_even
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+    assert add(-10, 5) == -5
+
+def test_is_even():
+    assert is_even(0) is True
+    assert is_even(2) is True
+    assert is_even(-4) is True
+    assert is_even(1) is False
+    assert is_even(-3) is False
